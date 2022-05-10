281719503ea1789758c116ee96f5cf1e56327b6c
Fede04colo <Frafede4c@gmail.com>
2022-05-10 08:32:46 +0200

    class CollisionChecker classe con gestione collisioni con le varie tile

--- main/src/main/CollisionChecker.java
+++ main/src/main/CollisionChecker.java
@@ -11,8 +11,9 @@ public class CollisionChecker {
 
     public void checkTile(Entity entity){
 
-        int entityRightWorldX = entity.worldX + entity.solidArea.x + entity.solidArea.width;
+
         int entityLeftWorldX = entity.worldX + entity.solidArea.x;
+        int entityRightWorldX = entity.worldX + entity.solidArea.x + entity.solidArea.width;
         int entityTopWorldY = entity.worldY + entity.solidArea.y;
         int entityBottomWorldY = entity.worldY + entity.solidArea.y + entity.solidArea.height;
 
