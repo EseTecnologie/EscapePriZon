1042de3b94cac3001ace5a4869de921d17d307c2
Fede04colo <Frafede4c@gmail.com>
2022-05-10 09:03:25 +0200

    classi con documentazione del codice

--- main/src/main/KeyHandler.java
+++ main/src/main/KeyHandler.java
@@ -1,16 +1,37 @@
+/**
+ * @author  federico colombo
+ * @version 1.0
+ * @file  KeyHandler.java
+ * @since 06/05/2022
+ * @brief gestione della classe KeyListener
+ *
+ */
 package main;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-
+/**
+ * @class KeyHandler
+ *
+ * @brief classe per la gestione del KeyListener
+ *
+ * questa classe controlla gli eventi keyPressed e keyReleased per gestire i comandi per il movimento del player
+ */
 public class KeyHandler implements KeyListener {
 
+    /** attributi booleani up, down, left e right che vengono settati a true se viene premuto il tasto corrispondente al movimento che deve effettuare*/
     public boolean upPressed, downPressed, leftPressed, rightPressed;
+
     @Override
     public void keyTyped(KeyEvent e) {
-
     }
+    /**
+     @brief metodo keyPressed che gestisce il click del tasto della tastiera
 
+    questo metodo serve per gestire il click su tastiera, assegna a true l'attributo corrispondente al tasto che viene schiacciato
+     controllando se il tasto cliccato è uno di quelli che servono al programma
+     @param  e parametro con tutte le informazioni del tasto che viene cliccato
+     */
     @Override
     public void keyPressed(KeyEvent e) {
         int code = e.getKeyCode();
@@ -28,7 +49,13 @@ public class KeyHandler implements KeyListener {
             rightPressed=true;
         }
     }
+    /**
+     @brief metodo keyReleased che gestisce il rilascio del tasto della tastiera
 
+     questo metodo serve per gestire il rilascio del tasto della tastiera, assegna a false l'attributo corrispondente al tasto che viene rilasciato
+     controllando se il tasto rilasciato è uno di quelli che servono al programma
+     @param  e parametro con tutte le informazioni del tasto che viene cliccato
+     */
     @Override
     public void keyReleased(KeyEvent e) {
         int code = e.getKeyCode();
