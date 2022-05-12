9e125b6bac1f95bee3ef0919b9017d4ee3ef22f7
Fede04colo <Frafede4c@gmail.com>
2022-05-12 19:16:25 +0200

    aggiunta due variabili

--- main/src/entity/Entity.java
+++ main/src/entity/Entity.java
@@ -12,5 +12,6 @@ public class Entity {
     public int spriteCounter = 0;
     public int spriteNum = 1;
     public Rectangle solidArea;
+    public int solidAreaDefaultX,solidAreaDefaultY;
     public boolean collisionOn = false;
 }
