cc08359046fe4217acbed14ad835e6f0ed4974d3
Fede04colo <Frafede4c@gmail.com>
2022-05-13 10:39:58 +0200

    Update

--- main/src/tile/TileManager.java
+++ main/src/tile/TileManager.java
@@ -91,6 +91,7 @@ public class TileManager {
             int screenX = worldX - gp.player.worldX + gp.player.screenX;
             int screenY = worldY - gp.player.worldY + gp.player.screenY;
 
+
             if (worldX + gp.tileSize > (gp.player.worldX - gp.player.screenX) &&
                     worldY + gp.tileSize > (gp.player.worldY - gp.player.screenY) &&
                     worldX - gp.tileSize < (gp.player.worldX + gp.player.screenX) &&
