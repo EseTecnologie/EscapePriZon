9f9964ebabc51791dd46f479d3278b62b065e6fe
Sottocasa Michele <michelesottocasa1@gmail.com>
2022-05-20 23:04:02 +0200

    Documenting code

--- src/main/UtilityTool.java
+++ src/main/UtilityTool.java
@@ -3,12 +3,26 @@ package main;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
+/**
+ * @author Sottocasa Michele
+ * @version 1.0
+ * @class UtilityTool
+ * @brief extra tools
+ */
 public class UtilityTool {
 
     public UtilityTool() {
     }
 
-    public BufferedImage scaleImage(BufferedImage original, int width, int height) {
+    /**
+     *
+     * @param original {@link BufferedImage} a dimensioni originali
+     * @param width {@link Integer} larghezza dell'immagine
+     * @param height {@link Integer} altezza dell'immagine
+     * @return {@link BufferedImage} immagine ingrandita
+     * @since 1.0
+     */
+    public BufferedImage scaleImage(BufferedImage original, Integer width, Integer height) {
         BufferedImage scaledImage = new BufferedImage(width, height, original.getType());
         Graphics2D g2 = scaledImage.createGraphics();
         g2.drawImage(original, 0, 0, width, height, null);
